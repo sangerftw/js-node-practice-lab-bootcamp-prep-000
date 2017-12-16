e47f8d9813dec2b3c51f7db5e761e9c5a44d9732
Sara <sangerftw@gmail.com>
2017-12-16 23:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 
 var name = "Sara"
 var height = "74"
-console.log(parseInt(height)-5)
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height)-5)
